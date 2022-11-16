1942106a3fdb8b04b05f21ec9be421cd5fac121b
Ivan Tkachuk <108585975+Frien72@users.noreply.github.com>
2022-11-16 21:02:56 +0500

    Инициализация приложения "заметки". *Создан отдел в ресурсах для хранения информации о заметках *Создан фрагмент основного отображения заметок *Фрагмент встроен в активити

---
+++ app/src/main/java/com/example/notes_pad/NoteTextFragment.java
@@ -0,0 +1,72 @@
+package com.example.notes_pad;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+
+public class NoteTextFragment extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    private String mParam1;
+    private String mParam2;
+
+    public NoteTextFragment() {
+
+    }
+
+    public static NotesFragment newInstance(String param1, String param2) {
+        NotesFragment fragment = new NotesFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_notes, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initList(view);
+    }
+    private void initList(View view) {
+        LinearLayout layoutView = (LinearLayout) view;
+        String[] noteText = getResources().getStringArray(R.array.note_text);
+
+        for (String notesText: noteText) {
+            TextView tv = new TextView(getContext());
+            tv.setText(notesText);
+            tv.setTextSize(25);
+            layoutView.addView(tv);
+        }
+    }
+}
